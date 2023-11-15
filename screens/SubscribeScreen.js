51c0d13a8c03476722b6f8aa7366dc5dda6bcec0
Larissa Bizzi <bizzi.larissa@gmail.com>
2023-11-15 11:06:36 -0500

    updating project, working on email validation

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,9 +1,85 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, Image, Text, StyleSheet, TextInput, Pressable, Alert } from 'react-native';
+import { validateEmail } from '../utils';
 
-const SubscribeScreen = () => {
-  // Add subscribe screen code here
-  return <View></View>;
-};
 
-export default SubscribeScreen;
+export default function SubscribeScreen () {
+  const [email, onChangeEmail] = useState('');
+  const createAlert = () =>
+    Alert.alert('Thanks for subscribing, stay tuned!', '', [
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  return (
+  <ScrollView style={styles.container}>
+    <Image
+      resizeMode={'contain'}
+      style={styles.logo}
+      source={require("../assets/little-lemon-logo.png")}
+      accessible={true}
+      accessibilityLabel='Little Lemon logo'
+    />
+    <Text style={styles.regularText}>
+      Subscribe to our newsletter for our latest delicious recipes!
+    </Text>
+    <TextInput
+        value={email}
+        onChangeText={onChangeEmail}
+        style={styles.inputBox}
+        placeholder='email'
+        keyboardType={'email-address'}
+        clearButtonMode='always'
+        validateEmail={email}
+/>
+    <Pressable
+        title={'Alert'}
+        onPress={createAlert}
+        style={styles.button}>
+         <Text style={styles.buttonText}>Subscribe</Text>
+    </Pressable>
+</ScrollView>
+);
+}
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+},
+regularText: {
+    fontSize: 24,
+    padding: 10,
+    marginVertical: 8,
+    color: '#495E57',
+    textAlign: 'center',
+},
+inputBox: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE',
+},
+button: {
+   fontSize: 22,
+   padding: 10,
+   marginVertical: 8,
+   margin: 100,
+   backgroundColor: '#F4CE14',
+   borderColor: '#F4CE14',
+   borderWidth: 2,
+   borderRadius: 50,
+},
+buttonText: {
+  color: 'black',
+  textAlign: 'center',
+  fontSize: 25,
+},
+logo: {
+  width: 200,
+  height: 200,
+  margin: 50,
+  alignSelf: 'center'
+},
+});
